eee5ffbbf1f6d3d56fdd4ac7a9eb16728a330866
shuchuangtech <435360@qq.com>
2015-11-10 22:14:16 +0800

    change JSONConfiguration test file to generate defaul config file

--- Src/Test/JSONConfiguration/main.cpp
+++ Src/Test/JSONConfiguration/main.cpp
@@ -2,37 +2,45 @@
 #include "Poco/JSON/Object.h"
 #include "Poco/JSON/Array.h"
 #include "Poco/Dynamic/VarHolder.h"
+#include "Poco/Dynamic/Struct.h"
 #include "Poco/AutoPtr.h"
 #include "Poco/FileStream.h"
 #include <stdio.h>
 #include <vector>
 using namespace Poco;
+void initDefaultConfig(JSON::Object::Ptr& pRoot)
+{
+	JSON::Object::Ptr pDeviceInfo = new JSON::Object;
+	pDeviceInfo->set("uuid", "SC0000000001");
+	pDeviceInfo->set("type", "sc-lock0001");
+	pDeviceInfo->set("name", "SC0000000001");
+	pDeviceInfo->set("manufacture", "Shuchuangtech");
+
+	JSON::Object::Ptr pRegProxy = new JSON::Object;
+	pRegProxy->set("host", "shuchuangtech.com");
+	pRegProxy->set("ssl_port", 12222);
+	pRegProxy->set("reg_port", 13333);
+
+	JSON::Array::Ptr pTasks = new JSON::Array;
+	//JSON::Object::Ptr nil = NULL;
+	//pTasks->add(nil);
+	JSON::Object::Ptr pNode = new JSON::Object;
+	pNode->set("Tasks", pTasks);
+	pNode->set("DeviceInfo", pDeviceInfo);
+	pNode->set("RegProxy", pRegProxy);
+
+	pRoot->set("root", pNode);
+}
+
 int main()
 {
-	
-	JSON::Object::Ptr obj = new JSON::Object;
-	DynamicStruct ds;
-	ds["attr1"] = "aaaa";
-	ds["attr2"] = "bbbb";
-	DynamicStruct param;
-	param["name"] = "huangjian";
-	param["age"] = 18;
-	ds["attr3"] = param;
-	JSON::Array array;
-	array.set(0u, "123");
-	array.set(1u,"321");
-	ds["attr4"] = array;
-	obj->set("Server", ds);
-	AutoPtr<Util::JSONConfiguration> conf = new Util::JSONConfiguration(obj);
 	FileOutputStream confFile("./test.conf");
+	JSON::Object::Ptr pObj = new JSON::Object;
+	initDefaultConfig(pObj);
+	DynamicStruct ds = *pObj;
+	printf("%s\n", ds.toString().c_str());
+	AutoPtr<Util::JSONConfiguration> conf = new Util::JSONConfiguration(pObj);
 	conf->save(confFile);
-	std::string str = conf->getRawString("Server");
-	printf("%s\n", str.c_str());
-	AutoPtr<Util::JSONConfiguration> conf2 = new Util::JSONConfiguration;
-	memcpy(conf2, conf, sizeof(Util::JSONConfiguration));
-	std::string str2 = conf2->getRawString("Server");
-	printf("%d\n", sizeof(Util::JSONConfiguration));
-	printf("%s\n", str2.c_str());
 	return 0;
 }
 
