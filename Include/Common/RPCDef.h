eb66e092d749fe533f90fad69ad766fd98201b80
shuchuangtech <435360@qq.com>
2015-11-10 14:23:31 +0800

    add Task macro definition

--- Include/Common/RPCDef.h
+++ Include/Common/RPCDef.h
@@ -11,6 +11,7 @@ request
 //×Ö¶ÎÃû
 #define KEY_TYPE_STR "type"
 #define KEY_ACTION_STR "action"
+#define KEY_TOKEN_STR "token"
 #define KEY_PARAM_STR "param"
 #define KEY_REQUEST_ID_STR "requestid"
 #define KEY_RESULT_STR "result"
@@ -20,23 +21,21 @@ request
 #define TYPE_RESPONSE_STR "response"
 //action×Ö¶Î component.method
 //param>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//user
 #define PARAM_USERNAME_STR "username"
 #define PARAM_PASSWORD_STR "password"
 #define PARAM_TOKEN_STR "token"
 #define PARAM_CHALLENGE_STR "challenge"
-#define PARAM_SESSION_STR "session"
-#define PARAM_HOST_STR "host"
-#define PARAM_PORT_STR "port"
+//register
 #define PARAM_UUID_STR "uuid"
 #define PARAM_DEV_NAME_STR "dev_name"
 #define PARAM_DEV_TYPE_STR "dev_type"
 #define PARAM_STATE_STR "state"
-
 #define PARAM_TIMESTAMP_STR "timestamp"
 #define PARAM_KEY_STR "key"
 //Component
-#define COMPONENT_CONN_STR "conn"
 #define COMPONENT_USER_STR "user"
+#define COMPONENT_TASK_STR "task"
 #define COMPONENT_DEVICE_STR "device"
 #define COMPONENT_SERVER_STR "server"
 //Method>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
@@ -44,9 +43,15 @@ request
 #define USER_METHOD_LOGIN "login"
 #define USER_METHOD_LOGOUT "logout"
 #define USER_METHOD_PASSWD "passwd"
+//task
+#define TASK_METHOD_ADD "add"
+#define TASK_METHOD_REMOVE "remove"
+#define TASK_METHOD_MODIFY "modify"
+#define TASK_METHOD_GETTASKS "gettasks"
 //device
 #define DEVICE_METHOD_OPEN "open"
 #define DEVICE_METHOD_CLOSE "close"
+#define DEVICE_METHOD_CHECK "check"
 //server
 #define SERVER_METHOD_CHECK "check"
 #define SERVER_METHOD_TOKEN "token"
