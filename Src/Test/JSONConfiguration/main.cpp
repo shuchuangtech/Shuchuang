686a072c58333b36dbf26040cb53706ad27764ff
shuchuangtech <435360@qq.com>
2015-11-12 19:42:53 +0800

    add JSONConfiguration server detault config

--- Src/Test/JSONConfiguration/main.cpp
+++ Src/Test/JSONConfiguration/main.cpp
@@ -8,35 +8,61 @@
 #include <stdio.h>
 #include <vector>
 using namespace Poco;
-void initDefaultConfig(JSON::Object::Ptr& pRoot)
+void initDefaultConfig(JSON::Object::Ptr& pRoot, int choice)
 {
-	JSON::Object::Ptr pDeviceInfo = new JSON::Object;
-	pDeviceInfo->set("uuid", "SC0000000001");
-	pDeviceInfo->set("type", "sc-lock0001");
-	pDeviceInfo->set("name", "SC0000000001");
-	pDeviceInfo->set("manufacture", "Shuchuangtech");
-
-	JSON::Object::Ptr pRegProxy = new JSON::Object;
-	pRegProxy->set("host", "shuchuangtech.com");
-	pRegProxy->set("ssl_port", 12222);
-	pRegProxy->set("reg_port", 13333);
-
-	JSON::Array::Ptr pTasks = new JSON::Array;
-	//JSON::Object::Ptr nil = NULL;
-	//pTasks->add(nil);
 	JSON::Object::Ptr pNode = new JSON::Object;
-	pNode->set("Tasks", pTasks);
-	pNode->set("DeviceInfo", pDeviceInfo);
-	pNode->set("RegProxy", pRegProxy);
+	if(choice == 1)
+	{
+		JSON::Object::Ptr pDeviceInfo = new JSON::Object;
+		pDeviceInfo->set("uuid", "SC0000000001");
+		pDeviceInfo->set("type", "sc-lock0001");
+		pDeviceInfo->set("name", "SC0000000001");
+		pDeviceInfo->set("manufacture", "Shuchuangtech");
+	
+		JSON::Object::Ptr pRegProxy = new JSON::Object;
+		pRegProxy->set("host", "shuchuangtech.com");
+		pRegProxy->set("ssl_port", 12222);
+		pRegProxy->set("reg_port", 13333);
+	
+		JSON::Array::Ptr pTasks = new JSON::Array;
+		//JSON::Object::Ptr nil = NULL;
+		//pTasks->add(nil);
+		pNode->set("Tasks", pTasks);
+		pNode->set("DeviceInfo", pDeviceInfo);
+		pNode->set("RegProxy", pRegProxy);
+	}
+	else if(choice == 2)
+	{
+		JSON::Object::Ptr pRegServer = new JSON::Object;
+		pRegServer->set("ssl_port", 12222);
+		pRegServer->set("reg_port", 13333);
 
+		JSON::Object::Ptr pHTTPSAcceptor = new JSON::Object;
+		pHTTPSAcceptor->set("port", 9888);
+		pHTTPSAcceptor->set("cert", "./cert.pem");
+		pHTTPSAcceptor->set("privkey", "privkey.pem");
+
+		JSON::Object::Ptr pHTTPServer = new JSON::Object;
+		pHTTPServer->set("port", 8777);
+
+		pNode->set("RegServer", pRegServer);
+		pNode->set("HTTPSAcceptor", pHTTPSAcceptor);
+		pNode->set("HTTPServer", pHTTPServer);
+	}
 	pRoot->set("root", pNode);
 }
 
 int main()
 {
+	int choice = 0;
+	printf("1. Generate device default config.\n"
+			"2. Generate server default config.\n");
+	int ret = scanf("%d", &choice);
+	if(ret < 0)
+		return 0;
 	FileOutputStream confFile("./test.conf");
 	JSON::Object::Ptr pObj = new JSON::Object;
-	initDefaultConfig(pObj);
+	initDefaultConfig(pObj, choice);
 	DynamicStruct ds = *pObj;
 	printf("%s\n", ds.toString().c_str());
 	AutoPtr<Util::JSONConfiguration> conf = new Util::JSONConfiguration(pObj);
