0b63e2c8477080682e148674f557d4022adb5b4e
huang_jian <huang_jian@hj.shuchuangtech>
2016-04-13 14:22:19 +0800

    设备模式设备测试

--- Src/Test/login/DoorOperation.cpp
+++ Src/Test/login/DoorOperation.cpp
@@ -71,14 +71,60 @@ void closeDoor()
 	}
 }
 
+void getMode()
+{
+	if(g_token.empty())
+	{
+		printf("Please login first.\n");
+		return;
+	}
+	DynamicStruct ds;
+	ds["type"] = "request";
+	ds["action"] = "device.getmode";
+	DynamicStruct param;
+	param["uuid"] = g_uuid;
+	param["token"] = g_token;
+	ds["param"] = param;
+	if(!sendRequest(ds.toString()))
+	{
+		warnf("%s, %d: Test faild\n", __FILE__, __LINE__);
+		return;
+	}
 
+}
+
+void changeMode()
+{
+	if(g_token.empty())
+	{
+		printf("Please login first.\n");
+		return;
+	}
+	DynamicStruct ds;
+	ds["type"] = "request";
+	ds["action"] = "device.chmode";
+	DynamicStruct param;
+	param["uuid"] = g_uuid;
+	param["token"] = g_token;
+	std::cout << "Mode:";
+	int mode;
+	std::cin >> mode;
+	param["mode"] = mode;
+	ds["param"] = param;
+	if(!sendRequest(ds.toString()))
+	{
+		warnf("%s, %d: Test faild\n", __FILE__, __LINE__);
+		return;
+	}
+
+}
 
 void showDoorOperation()
 {
 	while(1)
 	{
 		std::cout << "Action:" << std::endl << "1.Check" << std::endl << "2.Open" << std::endl;
-		std::cout << "3.Close" << std::endl << "0.Return" << std::endl;
+		std::cout << "3.Close" << std::endl << "4.Get mode" << std::endl << "5.Change mode" << std::endl << "0.Return" << std::endl;
 		int choice;
 		std::cin >> choice;
 		switch(choice)
@@ -91,6 +137,12 @@ void showDoorOperation()
 				break;
 			case 3:
 				closeDoor();
+				break;
+			case 4:
+				getMode();
+				break;
+			case 5:
+				changeMode();
 				break;
 			case 0:
 				return;
